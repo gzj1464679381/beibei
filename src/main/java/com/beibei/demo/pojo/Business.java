d4af9dcd10cdabede489288f0c1ad5b4adcbc14a
gzj1464679381 <43667736+gzj1464679381@users.noreply.github.com>
2018-12-17 16:07:16 +0800

    贝贝第一次Initial commit

---
+++ src/main/java/com/beibei/demo/pojo/Business.java
@@ -0,0 +1,26 @@
+package com.beibei.demo.pojo;
+//business   商家
+public class Business {
+
+    //商家id
+    private String id;
+    //商家名称
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/beibei/demo/pojo/User.java
@@ -0,0 +1,7 @@
+package com.beibei.demo.pojo;
+
+public class User {
+   private int id;
+   private String Phone;
+   private String password;
+}
