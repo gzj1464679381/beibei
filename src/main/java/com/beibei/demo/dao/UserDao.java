ea6a604e9f9289a75e3f1f486a20c4833ae4d561
Administrator <123>
2018-12-18 17:48:50 +0800

    双雄

---
+++ src/main/java/com/beibei/demo/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.beibei.demo.dao;
+
+import com.beibei.demo.pojo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserDao extends JpaRepository<User,Integer> {
+
+
+
+
+    
+
+
+}
