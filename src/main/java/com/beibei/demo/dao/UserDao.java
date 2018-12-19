1a042f703269b5044c90f2a898d75297e281b9a7
Administrator <123>
2018-12-19 12:11:11 +0800

    双雄

--- src/main/java/com/beibei/demo/dao/UserDao.java
+++ src/main/java/com/beibei/demo/dao/UserDao.java
@@ -1,16 +1,25 @@
 package com.beibei.demo.dao;
 
-import com.beibei.demo.pojo.User;
+import com.beibei.demo.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface UserDao extends JpaRepository<User,Integer> {
 
+    User findAllByy(String phone,String password);
+
+    int insert(@Param("phone") String phone,@Param("password") String password);
+
+
+    List<User> findAllBy();
 
 
 
-    
+
+
 
 
 }
