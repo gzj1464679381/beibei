667bf62644da53500da58c773856f4afd5a5526a
Administrator <123>
2018-12-20 17:07:55 +0800

    双雄

--- src/main/java/com/beibei/demo/DemoApplication.java
+++ src/main/java/com/beibei/demo/DemoApplication.java
@@ -1,11 +1,12 @@
 package com.beibei.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.beibei.demo.dao")
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
