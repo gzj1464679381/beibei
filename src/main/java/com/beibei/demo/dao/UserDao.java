85b0be5ee1c1f585b15c4116e5f6d3ea055aed79
Administrator <123>
2018-12-21 11:49:06 +0800

    双雄=====关于用户的数据层

--- src/main/java/com/beibei/demo/dao/UserDao.java
+++ src/main/java/com/beibei/demo/dao/UserDao.java
@@ -1,20 +1,29 @@
 package com.beibei.demo.dao;
 
 import com.beibei.demo.entity.User;
-import io.lettuce.core.dynamic.annotation.Param;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface UserDao extends JpaRepository<User,Integer> {
+public interface UserDao  {
 
-    User findAllByy(String phone,String password);
+    //把手机号和密码添加到数据库
+    int insert(@Param("phone") String phone, @Param("password") String password);
+    //根据用户名和密码查询
+    User findAllByy(@Param("phone") String phone,@Param("password") String password);
+    //根据用户名获取id
+    User findByphone(@Param("phone") String phone);
+    //修改密码
+    int update(@Param("password") String password,@Param("id") Integer id);
+    //查找全部
+    List<User> findAll();
+    //添加地址
+    int jia(@Param("addressName") String addressName,@Param("uid") Integer uid,@Param("val") Integer val);
 
-    int insert(@Param("phone") String phone,@Param("password") String password);
 
 
-    List<User> findAllBy();
+
 
 
 
